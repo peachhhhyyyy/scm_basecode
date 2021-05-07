8dae255f1145397a45c4b4755ab45f903d04d227
wold21 <wold2180@gmail.com>
2021-05-07 09:29:53 +0900

    출하계획 페이지 포인트

---
+++ src/main/java/kr/happyjob/study/dlv/controller/DlvOutgoingController.java
@@ -0,0 +1,21 @@
+package kr.happyjob.study.dlv.controller;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DlvOutgoingController {
+	
+	private final Logger logger = LogManager.getLogger(this.getClass());
+	
+	@RequestMapping("/dlv/outgoing.do")
+	public String initBoard() {
+		
+		logger.info("환영해");
+		
+		String returnType = "/dlv/outgoing";
+		return returnType;
+	}
+}
