2258328e52670aaf96728503e23782adc0ec096d
wold21 <wold2180@gmail.com>
2021-05-08 14:33:47 +0900

    수주리스트 조회 메소드

--- src/main/java/kr/happyjob/study/dlv/service/DlvOutgoingService.java
+++ src/main/java/kr/happyjob/study/dlv/service/DlvOutgoingService.java
@@ -3,7 +3,10 @@ package kr.happyjob.study.dlv.service;
 import java.util.List;
 import java.util.Map;
 
-public class DlvOutgoingService {
+import kr.happyjob.study.dlv.model.DlvOutgoingModel;
+
+public interface DlvOutgoingService {
 	
 	// 배송 준비 중 부터 리스트 조회
+	public List<DlvOutgoingModel> orderList(Map<String, Object> paramMap)throws Exception ;
 }
