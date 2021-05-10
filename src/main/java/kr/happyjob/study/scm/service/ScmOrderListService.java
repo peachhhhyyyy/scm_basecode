0b59c87ec86c91330b0c613009856425d105c7d1
rhange <rhange0511@gmail.com>
2021-05-10 18:20:37 +0900

    수주내역 조회 Service 구현

---
+++ src/main/java/kr/happyjob/study/scm/service/ScmOrderListService.java
@@ -0,0 +1,12 @@
+package kr.happyjob.study.scm.service;
+
+import java.util.List;
+import java.util.Map;
+
+import kr.happyjob.study.scm.model.ScmOrderListModel;
+
+public interface ScmOrderListService {
+
+	List<ScmOrderListModel> getOrderList(Map<String, Object> paramMap);
+
+}
---
+++ src/main/java/kr/happyjob/study/scm/service/ScmOrderListServiceImpl.java
@@ -0,0 +1,31 @@
+package kr.happyjob.study.scm.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.happyjob.study.scm.dao.ScmOrderListDao;
+import kr.happyjob.study.scm.model.ScmOrderListModel;
+
+@Service
+public class ScmOrderListServiceImpl implements ScmOrderListService {
+
+	// Set logger
+	private final Logger logger = LogManager.getLogger(this.getClass());
+
+	// Get class name for logger
+	private final String className = this.getClass().toString();
+
+	@Autowired
+	ScmOrderListDao ScmOrderListDao;
+
+	@Override
+	public List<ScmOrderListModel> getOrderList(Map<String, Object> paramMap) throws Exception {
+		return ScmOrderListDao.selectOrderList(paramMap);
+	}
+
+}
