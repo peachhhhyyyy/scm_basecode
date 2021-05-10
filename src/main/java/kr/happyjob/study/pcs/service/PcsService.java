ade6e2a764e6bb0350cf74b0716d8c437f9a9727
peachhhhyyyy <peachhhhyyyy@gmail.com>
2021-05-10 14:33:55 +0900

    PcsService 이클립스 수정사항으로 변경

--- src/main/java/kr/happyjob/study/pcs/service/PcsService.java
+++ src/main/java/kr/happyjob/study/pcs/service/PcsService.java
@@ -7,7 +7,7 @@ import kr.happyjob.study.pcs.model.PcsModel;
 
 public interface PcsService {
   /** 그룹코드 목록 조회 */
-  public List<PcsModel> listComnGrpCod2(Map<String, Object> paramMap);
+  public List<PcsModel> listComnGrpCod2(Map<String, Object> paramMap) throws Exception;
   
   /** 그룹코드 목록 카운트 조회 */
   public int countListComnGrpCod2(Map<String, Object> paramMap) throws Exception;
--- src/main/java/kr/happyjob/study/pcs/service/PcsServiceImpl.java
+++ src/main/java/kr/happyjob/study/pcs/service/PcsServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import kr.happyjob.study.pcs.dao.PcsDao;
 import kr.happyjob.study.pcs.model.PcsModel;
+import kr.happyjob.study.pcs.service.PcsService;
 
 @Service
 public class PcsServiceImpl implements PcsService {
@@ -23,27 +24,16 @@ public class PcsServiceImpl implements PcsService {
   @Autowired
   PcsDao pcsDao;
   
-  /** 그룹코드 목록 조회 */
-  public List<PcsModel> listComnGrpCod(Map<String, Object> paramMap) throws Exception {
-    List<PcsModel> listComnGrpCod = pcsDao.listComnGrpCod2(paramMap);
+  /** 그룹코드 목록 조회 
+   * @throws Exception */
+  public List<PcsModel> listComnGrpCod2(Map<String, Object> paramMap) throws Exception {
+    List<PcsModel> listComnGrpCod = PcsDao.listComnGrpCod2(paramMap);
     return listComnGrpCod;
   }
   
   /** 그룹코드 목록 카운트 조회 */
-  public int countListComnGrpCod(Map<String, Object> paramMap) throws Exception {
+  public int countListComnGrpCod2(Map<String, Object> paramMap) throws Exception {
     int totalCount = PcsDao.countListComnGrpCod2(paramMap);
     return totalCount;
-  }
-
-  @Override
-  public List<PcsModel> listComnGrpCod2(Map<String, Object> paramMap) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public int countListComnGrpCod2(Map<String, Object> paramMap) throws Exception {
-    // TODO Auto-generated method stub
-    return 0;
   }
 }
