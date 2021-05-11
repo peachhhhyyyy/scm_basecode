2e321794260a17dbc7f0b05a6ff2984e877e4ef1
sjinicd <sjinicd5@gmail.com>
2021-05-11 14:39:33 +0900

    DivOutgoingController 커밋테스트

--- src/main/java/kr/happyjob/study/dlv/controller/DlvOutgoingController.java
+++ src/main/java/kr/happyjob/study/dlv/controller/DlvOutgoingController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
