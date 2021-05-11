9246a3ff0cdde47aeee1b46bc673ae2662702670
PSSU <sangsu9701@naver.com>
2021-05-11 09:20:09 +0900

    add ctm java, sql, jsp

---
+++ src/main/java/kr/happyjob/study/ctm/service/OrderService.java
@@ -0,0 +1,12 @@
+package kr.happyjob.study.ctm.service;
+
+import java.util.List;
+import java.util.Map;
+
+import kr.happyjob.study.dashboard.model.DeliveryBuyerModel;
+
+public interface OrderService {
+	
+	List<DeliveryBuyerModel> deliveryBuyerList(Map<String, Object> paramMap);
+
+}
