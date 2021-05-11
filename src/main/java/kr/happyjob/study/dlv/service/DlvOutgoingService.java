cdec18623c2a62e4f9cd62c176528886ba8ffdf8
wold21 <wold2180@gmail.com>
2021-05-11 14:43:07 +0900

    서비스 단 상세조회 메소드 작성

--- src/main/java/kr/happyjob/study/dlv/service/DlvOutgoingService.java
+++ src/main/java/kr/happyjob/study/dlv/service/DlvOutgoingService.java
@@ -3,6 +3,7 @@ package kr.happyjob.study.dlv.service;
 import java.util.List;
 import java.util.Map;
 
+import kr.happyjob.study.dlv.model.DlvOutgoingDetailModel;
 import kr.happyjob.study.dlv.model.DlvOutgoingModel;
 
 public interface DlvOutgoingService {
@@ -12,5 +13,8 @@ public interface DlvOutgoingService {
 	
 	// 배송 준비 중 부터 리스트 카운트 조회
 	public int outgoingCnt(Map<String, Object> paramMap)throws Exception ;
+	
+	// 배송 준비 중 부터 한건에 대한 상세 조회
+	public List<DlvOutgoingDetailModel> outgoingDetailList(Map<String, Object> paramMap)throws Exception ;
 	
 }
