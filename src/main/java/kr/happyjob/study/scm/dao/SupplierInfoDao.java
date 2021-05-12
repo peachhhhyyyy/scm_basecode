88cdc0af099c1f8492c14da2c678b89df0a81e43
LEEKEUNSANG <leekeunsang1994@gmail.com>
2021-05-12 10:44:26 +0900

    SupplierInfoDao.java 추가
    목록 조회, 목록 카운트 조회 추가

--- src/main/java/kr/happyjob/study/scm/dao/SupplierInfoDao.java
+++ src/main/java/kr/happyjob/study/scm/dao/SupplierInfoDao.java
@@ -1,5 +1,14 @@
 package kr.happyjob.study.scm.dao;
 
-public class SupplierInfoDao {
+import java.util.List;
+import java.util.Map;
+
+import kr.happyjob.study.scm.model.SupplierInfoModel;
+
+public interface SupplierInfoDao {
+  /* 목록 조회 */
+  public List<SupplierInfoModel> getSupplierInfo(Map<String, Object> paramMap) throws Exception;
   
+  /* 목록 카운트 조회*/
+  public List<SupplierInfoModel> countSupplierInfo(Map<String, Object> paramMap) throws Exception;
 }
