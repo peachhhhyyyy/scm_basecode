b2e2b6fe150c2222ac9237df218572fece3382fb
LEEKEUNSANG <leekeunsang1994@gmail.com>
2021-05-12 17:03:54 +0900

    SupplierInfoModel.java 추가
    공급처 목록 조회

---
+++ src/main/java/kr/happyjob/study/scm/model/SupplierInfoModel.java
@@ -0,0 +1,57 @@
+package kr.happyjob.study.scm.model;
+
+public class SupplierInfoModel {
+  
+  private String supply_cd;
+  private String supply_nm;
+  private String supply_mng_nm;
+  private String tel;
+  private String email;
+  private String warehouse_cd;
+  private String warehouse_nm;
+  
+  
+  public String getSupply_cd() {
+    return supply_cd;
+  }
+  public void setSupply_cd(String supply_cd) {
+    this.supply_cd = supply_cd;
+  }
+  
+  public String getSupply_nm() {
+    return supply_nm;
+  }
+  public void setSupply_nm(String supply_nm) {
+    this.supply_nm = supply_nm;
+  }
+  public String getSupply_mng_nm() {
+    return supply_mng_nm;
+  }
+  public void setSupply_mng_nm(String supply_mng_nm) {
+    this.supply_mng_nm = supply_mng_nm;
+  }
+  public String getTel() {
+    return tel;
+  }
+  public void setTel(String tel) {
+    this.tel = tel;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public String getWarehouse_cd() {
+    return warehouse_cd;
+  }
+  public void setWarehouse_cd(String warehouse_cd) {
+    this.warehouse_cd = warehouse_cd;
+  }
+  public String getWarehouse_nm() {
+    return warehouse_nm;
+  }
+  public void setWarehouse_nm(String warehouse_nm) {
+    this.warehouse_nm = warehouse_nm;
+  }
+}
