25b3f1aa5396c04ec648714d65acca0efc93ca77
LEEKEUNSANG <leekeunsang1994@gmail.com>
2021-05-12 12:52:19 +0900

    Revert "SupplierInfoDao.java 추가"
    This reverts commit 88cdc0af099c1f8492c14da2c678b89df0a81e43.

--- src/main/java/kr/happyjob/study/scm/dao/SupplierInfoDao.java
+++ src/main/java/kr/happyjob/study/scm/dao/SupplierInfoDao.java
@@ -1,14 +1,5 @@
 package kr.happyjob.study.scm.dao;
 
-import java.util.List;
-import java.util.Map;
-
-import kr.happyjob.study.scm.model.SupplierInfoModel;
-
-public interface SupplierInfoDao {
-  /* 목록 조회 */
-  public List<SupplierInfoModel> getSupplierInfo(Map<String, Object> paramMap) throws Exception;
+public class SupplierInfoDao {
   
-  /* 목록 카운트 조회*/
-  public List<SupplierInfoModel> countSupplierInfo(Map<String, Object> paramMap) throws Exception;
 }
