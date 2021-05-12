5fc88a9aae46eb9ea977d683bcd47f0fd50a4e98
LEEKEUNSANG <leekeunsang1994@gmail.com>
2021-05-12 20:18:31 +0900

    제품정보 리스트 Dao 추가
    오류나서 고쳐야함

--- src/main/java/kr/happyjob/study/scm/dao/SupplierInfoDao.java
+++ src/main/java/kr/happyjob/study/scm/dao/SupplierInfoDao.java
@@ -6,7 +6,14 @@ import java.util.Map;
 import kr.happyjob.study.scm.model.SupplierInfoModel;
 
 public interface SupplierInfoDao {
-  public List<SupplierInfoModel> getSupplierInfo(Map<String, Object> paramMap) throws Exception;
   
+  // 공급처 목록 조회
+  public List<SupplierInfoModel> getSupplierInfo(Map<String, Object> paramMap) throws Exception;
+  // 공급처 목록 갯수 조회
   public int countSupplierInfo(Map<String, Object> paramMap) throws Exception;
+  
+  // 제품정보 목록 조회
+  public List<SupplierInfoModel> supplierProList(Map<String, Object> paraMap) throws Exception;
+  // 제품정보 목록 갯수 조회
+  public int totalCntProduct(Map<String, Object> paramMap) throws Exception;
 }
