57c6084086b076407fea6eee2d33a2b1b6df651f
rhange <rhange0511@gmail.com>
2021-05-12 12:05:30 +0900

    tingle 모달창 만들어보기
    - tingle 모달창은 객체로 만들어져 있으며 각 인스턴스를 생성하여 모달을 사용 할 수 있다.
    - 기본 디자인을 .css 파일을 수정하여서 현재 프로젝트의 느낌이 나도록 수정함. (버튼 디자인)
    - 승인/취소 버튼 한글로 바꿈
    
    여기서부터 할 일들
    - `purchaseModalOpen()` 함수의 첫 부분에 callAjax 함수를 넣어서 선택한 주문에 해당하는 재고번호(재고명)을 SQL에서 가져와서 모달창에 띄울 것임.
    - 가져온 데이터를 '발주지시서 테이블'에 insert 할 예정

--- src/main/webapp/WEB-INF/resource/js/view/scm/orderListHistory/orderList.js
+++ src/main/webapp/WEB-INF/resource/js/view/scm/orderListHistory/orderList.js
@@ -5,6 +5,7 @@ var pageBlockSizeOrderList = 10;
 $(document).ready(function() {
 	// 일일 수주 조회
 	forderListHistory();
+	
 });
 
 //function fExcelDownload(tableID, fileID){
@@ -67,9 +68,56 @@ function forderListHistoryResult(data, currentPage) {
 // 입금대기 상태일때 배송,발주 버튼 클릭할 때 나타나는 에러 메시지 함수
 function fnotYetDeposit() {
   swal("아직 입금이 완료되지 않았습니다.");
+  return;
 };
 
 function fnotEnoughStock() {
-  swal("재고량이 주문 수량보다 적습니다. \n발주를 진행하여 재고 입고를 진행해주세요.")
+  swal("재고량이 주문 수량보다 적습니다. \n발주를 진행하여 재고 입고를 진행해주세요.");
+  return;
 };
 
+// 모달창 만들기
+// instanciate new modal
+// 구매버튼 모달창 만들기
+var purchaseModal = new tingle.modal({
+    footer: true,
+    stickyFooter: false,
+    closeMethods: ['overlay', 'button', 'escape'],
+    closeLabel: "Close",
+    cssClass: ['custom-class-1', 'custom-class-2'],
+    onOpen: function() {
+        console.log('modal open');
+    },
+    onClose: function() {
+        console.log('modal closed');
+    },
+    beforeClose: function() {
+        // here's goes some logic
+        // e.g. save content before closing the modal
+        return true; // close the modal
+        return false; // nothing happens
+    }
+});
+
+// add a button
+purchaseModal.addFooterBtn('승인요청', 'tingle-btn tingle-btn--primary', function() {
+    // here goes some logic
+  purchaseModal.close();
+});
+
+// add another button
+purchaseModal.addFooterBtn('취소', 'tingle-btn tingle-btn--danger', function() {
+  purchaseModal.close();
+});
+
+function purchaseModalOpen() {
+  //set content
+  purchaseModal.setContent(getPurchaseDirectionPage());
+  // 모달창 열기
+  purchaseModal.open();
+}
+
+function getPurchaseDirectionPage() {
+  console.log("통과");
+  return "Hi";
+}
