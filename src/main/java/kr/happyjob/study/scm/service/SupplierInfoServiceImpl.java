4017a8cf80975c45ffd89fec546d6b3a029d7d4e
LEEKEUNSANG <leekeunsang1994@gmail.com>
2021-05-12 10:41:02 +0900

    SupplierInfoService.java및 Impl.java 추가
    화면목록 리스트, 카운트 추가

--- src/main/java/kr/happyjob/study/scm/service/SupplierInfoServiceImpl.java
+++ src/main/java/kr/happyjob/study/scm/service/SupplierInfoServiceImpl.java
@@ -1,5 +1,29 @@
 package kr.happyjob.study.scm.service;
 
-public class SupplierInfoServiceImpl {
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.happyjob.study.scm.dao.SupplierInfoDao;
+import kr.happyjob.study.scm.model.SupplierInfoModel;
+
+@Service
+public class SupplierInfoServiceImpl implements SupplierInfoService {
   
+  @Autowired
+  SupplierInfoDao supplierInfoDao;
+  
+  @override
+  public List<SupplierInfoModel> getSupplierInfo (Map<String, Object> paramMap) throws Exception {
+    List<SupplierInfoModel> supplierList = supplierInfoDao.getSupplerInfo(paramMap);
+    return supplierList;
+  }
+  
+  @override
+  public int countSupplierInfo(Map<String, Object> paramMap) throws Exception {
+    int supplierCount = supplierInfoDao.countsupplierInfo(paramMap);
+    return supplierCount;
+  }
 }
