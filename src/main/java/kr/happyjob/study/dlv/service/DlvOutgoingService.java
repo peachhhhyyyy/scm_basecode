04cd8061c8b2a112c4cf8dd12b3c551ee4ae5b2d
wold21 <wold2180@gmail.com>
2021-05-13 17:51:45 +0900

    검색조건 서비스 메소드 작성

--- src/main/java/kr/happyjob/study/dlv/service/DlvOutgoingService.java
+++ src/main/java/kr/happyjob/study/dlv/service/DlvOutgoingService.java
@@ -5,16 +5,25 @@ import java.util.Map;
 
 import kr.happyjob.study.dlv.model.DlvOutgoingDetailModel;
 import kr.happyjob.study.dlv.model.DlvOutgoingModel;
+import kr.happyjob.study.dlv.model.DlvStaffNameModel;
 
 public interface DlvOutgoingService {
 	
 	// 배송 준비 중 부터 리스트 조회
 	public List<DlvOutgoingModel> outgoingList(Map<String, Object> paramMap)throws Exception ;
 	
-	// 배송 준비 중 부터 리스트 카운트 조회
+	// 배송 준비 중 부터 리스트 건수
 	public int outgoingCnt(Map<String, Object> paramMap)throws Exception;
+	
+	// 검색 조건 출하내역 조회
+	public List<DlvOutgoingModel> outgoingSearchList(Map<String, Object> paramMap) throws Exception ;
+	
+	// 검색 조건 출하내역 건수
+	public int outgoingSearchCnt(Map<String, Object> paramMap)throws Exception;
 	
 	// 배송 준비 중 부터 한건에 대한 상세 조회
 	public List<DlvOutgoingDetailModel> outgoingDetailList(Map<String, Object> paramMap)throws Exception ;
 	
+	// 배송 사원 이름 조회
+	public List<DlvStaffNameModel> dlvStaffNameCombo(Map<String, Object> paramMap)throws Exception ;
 }
