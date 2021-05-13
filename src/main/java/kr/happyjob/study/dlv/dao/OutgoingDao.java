7d7ebcfce704ac8cd633f1ba5197fbca47899a9e
wold21 <wold2180@gmail.com>
2021-05-13 17:49:34 +0900

    검색조건에 맞게 메소드 생성

--- src/main/java/kr/happyjob/study/dlv/dao/OutgoingDao.java
+++ src/main/java/kr/happyjob/study/dlv/dao/OutgoingDao.java
@@ -5,15 +5,27 @@ import java.util.Map;
 
 import kr.happyjob.study.dlv.model.DlvOutgoingDetailModel;
 import kr.happyjob.study.dlv.model.DlvOutgoingModel;
+import kr.happyjob.study.dlv.model.DlvStaffNameModel;
 
 public interface OutgoingDao {
 
 	// 1. 배송 준비 중 부터의 리스트 조회 / 상세조회 항목 포함.
 	public List<DlvOutgoingModel> outgoingList(Map<String, Object> paramMap)throws Exception ;
 	
-	// 1-1 리스트 목록 카운트 조회
+	// 리스트 목록 건수 조회
 	public int outgoingCnt(Map<String, Object> paramMap)throws Exception;
 	
-	// 1-2 주문코드에 대한 상세조회
+	// 검색 조건 출하내역 Dao
+	public List<DlvOutgoingModel> outgoingSearchList(Map<String, Object> paramMap)throws Exception ;
+	
+	// 검색 조건 출하내역 건수
+	public int outgoingSearchCnt(Map<String, Object> paramMap)throws Exception;
+	
+	// 주문코드에 대한 상세조회
 	public List<DlvOutgoingDetailModel> outgoingDetailList(Map<String, Object> paramMap)throws Exception ;
+	
+	// 배송사원이름 조회 -> 콤보박스로 감.
+	public List<DlvStaffNameModel> dlvStaffNameCombo(Map<String, Object> paramMap)throws Exception ;
+	
+	
 }
