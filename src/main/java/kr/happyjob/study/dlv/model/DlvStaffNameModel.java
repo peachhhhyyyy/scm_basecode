edd620c04f7afdee571e985b8d3ab42da648aba6
wold21 <wold2180@gmail.com>
2021-05-13 17:51:09 +0900

    배송기사 조회 모델 작성

---
+++ src/main/java/kr/happyjob/study/dlv/model/DlvStaffNameModel.java
@@ -0,0 +1,15 @@
+package kr.happyjob.study.dlv.model;
+
+public class DlvStaffNameModel {
+	
+	private String dlv_staff_name;
+
+	public String getDlv_staff_name() {
+		return dlv_staff_name;
+	}
+
+	public void setDlv_staff_name(String dlv_staff_name) {
+		this.dlv_staff_name = dlv_staff_name;
+	}
+	
+}
