62b2bf8f1d7ecdc855c2598f2416e8c42f9f58f9
wold21 <wold2180@gmail.com>
2021-05-15 12:55:51 +0900

    배송사원 ID 변수 생성 및 gatter setter생성

--- src/main/java/kr/happyjob/study/dlv/model/DlvStaffNameModel.java
+++ src/main/java/kr/happyjob/study/dlv/model/DlvStaffNameModel.java
@@ -3,6 +3,8 @@ package kr.happyjob.study.dlv.model;
 public class DlvStaffNameModel {
 	
 	private String dlv_staff_name;
+	
+	private String loginID;
 
 	public String getDlv_staff_name() {
 		return dlv_staff_name;
@@ -11,5 +13,20 @@ public class DlvStaffNameModel {
 	public void setDlv_staff_name(String dlv_staff_name) {
 		this.dlv_staff_name = dlv_staff_name;
 	}
+
+	public String getLoginID() {
+		return loginID;
+	}
+
+	public void setLoginID(String loginID) {
+		this.loginID = loginID;
+	}
+
+	@Override
+	public String toString() {
+		return "DlvStaffNameModel [dlv_staff_name=" + dlv_staff_name + ", loginID=" + loginID + "]";
+	}
+	
+	
 	
 }
