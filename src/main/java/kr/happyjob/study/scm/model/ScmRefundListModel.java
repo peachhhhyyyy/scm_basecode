4050cec531f727c7d10e606f8cea028cf71bad43
rhange <rhange0511@gmail.com>
2021-05-18 14:57:56 +0900

    SCM 반품신청목록 Model 구현

---
+++ src/main/java/kr/happyjob/study/scm/model/ScmRefundListModel.java
@@ -0,0 +1,78 @@
+package kr.happyjob.study.scm.model;
+
+public class ScmRefundListModel {
+
+	String orderCode;
+	String customerName;
+	String productName;
+	String middleCategory;
+	String refundDate;
+	int refundCount;
+	int refundAmount;
+	int STTcd;
+
+	public String getOrderCode() {
+		return orderCode;
+	}
+
+	public void setOrderCode(String orderCode) {
+		this.orderCode = orderCode;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getMiddleCategory() {
+		return middleCategory;
+	}
+
+	public void setMiddleCategory(String middleCategory) {
+		this.middleCategory = middleCategory;
+	}
+
+	public String getRefundDate() {
+		return refundDate;
+	}
+
+	public void setRefundDate(String refundDate) {
+		this.refundDate = refundDate;
+	}
+
+	public int getRefundCount() {
+		return refundCount;
+	}
+
+	public void setRefundCount(int refundCount) {
+		this.refundCount = refundCount;
+	}
+
+	public int getRefundAmount() {
+		return refundAmount;
+	}
+
+	public void setRefundAmount(int refundAmount) {
+		this.refundAmount = refundAmount;
+	}
+
+	public int getSTTcd() {
+		return STTcd;
+	}
+
+	public void setSTTcd(int sTTcd) {
+		STTcd = sTTcd;
+	}
+
+}
