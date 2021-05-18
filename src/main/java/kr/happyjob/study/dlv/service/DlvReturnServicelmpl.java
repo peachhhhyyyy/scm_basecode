f9705bc3ae7440ead2c5f5bdc413a01993631883
wold21 <wold2180@gmail.com>
2021-05-18 22:25:48 +0900

    서비스 변경에 따른 구현 메소드 오버라이드

--- src/main/java/kr/happyjob/study/dlv/service/DlvReturnServicelmpl.java
+++ src/main/java/kr/happyjob/study/dlv/service/DlvReturnServicelmpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import kr.happyjob.study.dlv.dao.ReturnDao;
 import kr.happyjob.study.dlv.model.DlvReturnDetailModel;
 import kr.happyjob.study.dlv.model.DlvReturnModel;
+import kr.happyjob.study.dlv.model.DlvStaffNameModel;
 
 @Service
 public class DlvReturnServicelmpl implements DlvReturnService {
@@ -16,6 +17,7 @@ public class DlvReturnServicelmpl implements DlvReturnService {
 	
 	@Autowired
 	ReturnDao returndao;
+	
 	@Override
 	public List<DlvReturnModel> returnList(Map<String, Object> paramMap) throws Exception {
 		List<DlvReturnModel> returnList = returndao.returnList(paramMap);
@@ -24,9 +26,18 @@ public class DlvReturnServicelmpl implements DlvReturnService {
 
 	@Override
 	public int returnlistCnt(Map<String, Object> paramMap) throws Exception {
-		
 		int totalCount = returndao.returnListCnt(paramMap);
 		return totalCount;
+	}
+	
+	@Override
+	public List<DlvReturnModel> returnSearchList(Map<String, Object> paramMap) throws Exception {
+		return returndao.returnSearchList(paramMap);
+	}
+	
+	@Override
+	public int returnSearchCnt(Map<String, Object> paramMap) throws Exception {
+		return returndao.returnSearchCnt(paramMap);
 	}
 
 	@Override
@@ -39,6 +50,21 @@ public class DlvReturnServicelmpl implements DlvReturnService {
 	public int returnDetailInfo(Map<String, Object> paramMap) throws Exception {
 		int returnDetailInfo = returndao.returnDetailInfo(paramMap);
 		return returnDetailInfo;
+	}
+
+	@Override
+	public List<DlvStaffNameModel> dlvStaffNameCombo(Map<String, Object> paramMap) throws Exception {
+		return returndao.dlvStaffNameCombo(paramMap);
+	}
+
+	@Override
+	public String getDlvStaffTel(Map<String, Object> paramMap) throws Exception {
+		return returndao.getDlvStaffTel(paramMap); 
+	}
+
+	@Override
+	public int updateReturnPaper(Map<String, Object> paramMap) throws Exception {
+		return returndao.updateReturnPaper(paramMap);
 	}
 
 }
