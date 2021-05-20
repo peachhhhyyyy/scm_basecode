5e6ac554f9d7e8710d542426cf4995b345b00445
wold21 <wold2180@gmail.com>
2021-05-20 15:46:46 +0900

    변경사항  - 가져올 변수 새로 작성 및 getter setter 추가

--- src/main/java/kr/happyjob/study/scm/model/ScmReturnListModel.java
+++ src/main/java/kr/happyjob/study/scm/model/ScmReturnListModel.java
@@ -1,80 +1,96 @@
 package kr.happyjob.study.scm.model;
 
-import java.util.Date;
 
 public class ScmReturnListModel {
-	//반품지시서 번호
-	private String refund_list_no;
-	//고객명
-	private String cus_name;
-	//제품명
-	private String prod_nm;
-	//분류명
-	private String m_ct_cd;
-	//반품개수
-	private int return_qty;
-	//scm관리자
-	private String scm_id;
-	//제출일자
-	private Date submit_date;
 	
-	public ScmReturnListModel() {
-	}
-
+	// 반품지시서 번호(index)
+	private String refund_list_no;
+	// 고객명 
+	private String cus_nm;
+	// 품목명 
+	private String l_ct_nm;
+	// 제품명 
+	private String prod_nm;
+	// 반품 개수 
+	private String return_cnt;
+	// SCM 담당자 이름 
+	private String scm_nm;
+	// 제출일자 
+	private String submit_date;
+	// 창고명 
+	private String warehouse_nm;
+	// 반품 총금액 
+	private String refund_amt;
+	// 승인ID(임원ID)
+	private String approve_nm;
+	// 공급처명 
+	private String supply_nm;
+	
 	public String getRefund_list_no() {
 		return refund_list_no;
 	}
-
 	public void setRefund_list_no(String refund_list_no) {
 		this.refund_list_no = refund_list_no;
 	}
-
-	public String getCus_name() {
-		return cus_name;
+	public String getCus_nm() {
+		return cus_nm;
 	}
-
-	public void setCus_name(String cus_name) {
-		this.cus_name = cus_name;
+	public void setCus_nm(String cus_nm) {
+		this.cus_nm = cus_nm;
 	}
-
+	public String getL_ct_nm() {
+		return l_ct_nm;
+	}
+	public void setL_ct_nm(String l_ct_nm) {
+		this.l_ct_nm = l_ct_nm;
+	}
 	public String getProd_nm() {
 		return prod_nm;
 	}
-
 	public void setProd_nm(String prod_nm) {
 		this.prod_nm = prod_nm;
 	}
-
-	public String getM_ct_cd() {
-		return m_ct_cd;
+	public String getReturn_cnt() {
+		return return_cnt;
 	}
-
-	public void setM_ct_cd(String m_ct_cd) {
-		this.m_ct_cd = m_ct_cd;
+	public void setReturn_cnt(String return_cnt) {
+		this.return_cnt = return_cnt;
 	}
-
-	public int getReturn_qty() {
-		return return_qty;
+	public String getScm_nm() {
+		return scm_nm;
 	}
-
-	public void setReturn_qty(int return_qty) {
-		this.return_qty = return_qty;
+	public void setScm_nm(String scm_nm) {
+		this.scm_nm = scm_nm;
 	}
-
-	public String getScm_id() {
-		return scm_id;
-	}
-
-	public void setScm_id(String scm_id) {
-		this.scm_id = scm_id;
-	}
-
-	public Date getSubmit_date() {
+	public String getSubmit_date() {
 		return submit_date;
 	}
-
-	public void setSubmit_date(Date submit_date) {
+	public void setSubmit_date(String submit_date) {
 		this.submit_date = submit_date;
+	}
+	public String getWarehouse_nm() {
+		return warehouse_nm;
+	}
+	public void setWarehouse_nm(String warehouse_nm) {
+		this.warehouse_nm = warehouse_nm;
+	}
+	public String getRefund_amt() {
+		return refund_amt;
+	}
+	public void setRefund_amt(String refund_amt) {
+		this.refund_amt = refund_amt;
+	}
+	public String getApprove_nm() {
+		return approve_nm;
+	}
+	public void setApprove_nm(String approve_nm) {
+		this.approve_nm = approve_nm;
+	}
+	public String getSupply_nm() {
+		return supply_nm;
+	}
+	public void setSupply_nm(String supply_nm) {
+		this.supply_nm = supply_nm;
 	}
 	
 }
