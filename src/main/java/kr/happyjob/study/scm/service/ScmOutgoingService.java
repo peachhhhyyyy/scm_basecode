55b2a24345d78662a4f4d1b3097ea0e844984338
wold21 <wold2180@gmail.com>
2021-05-21 14:00:42 +0900

    변경사항  - 검색 조건 및 상세조회 메소드 생성

--- src/main/java/kr/happyjob/study/scm/service/ScmOutgoingService.java
+++ src/main/java/kr/happyjob/study/scm/service/ScmOutgoingService.java
@@ -3,11 +3,18 @@ package kr.happyjob.study.scm.service;
 import java.util.List;
 import java.util.Map;
 
+import kr.happyjob.study.scm.model.ScmOutgoingDetailListModel;
 import kr.happyjob.study.scm.model.ScmOutgoingListModel;
 
 public interface ScmOutgoingService {
-	//반품지시서 조회
+	// 배송지시서 조회
 	public List<ScmOutgoingListModel> getOutgoingList(Map<String, Object> paramMap) throws Exception;
-	//반품지시서 카운트조회
+	// 배송지시서 카운트조회
 	public int scmOutgoingListCnt(Map<String, Object> paramMap) throws Exception;
+	// 배송지시서 검색조건 조회
+	public List<ScmOutgoingListModel> getOutgoingSearchList(Map<String, Object> paramMap) throws Exception;
+	// 배송지시서 카운트조회
+	public int scmOutgoingSearchListCnt(Map<String, Object> paramMap) throws Exception;
+	// 배송지시서 상조회
+	public List<ScmOutgoingDetailListModel> getOutgoingDetailList(Map<String, Object> paramMap) throws Exception;
 }
