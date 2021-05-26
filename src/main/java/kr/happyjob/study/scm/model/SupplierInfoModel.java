f64b3e798a14321ba6e4198f03223a0e099bf85b
LEEKEUNSANG <leekeunsang1994@gmail.com>
2021-05-26 13:36:01 +0900

    줄맞춤 정리

--- src/main/java/kr/happyjob/study/scm/model/SupplierInfoModel.java
+++ src/main/java/kr/happyjob/study/scm/model/SupplierInfoModel.java
@@ -7,7 +7,7 @@ public class SupplierInfoModel {
   private String supply_nm;
   // 공급처 담당자명
   private String supply_mng_nm;
-  // 공급처  연락처
+  // 공급처 연락처
   private String tel;
   // 공급처 담당자 연락처
   private String mng_tel;
@@ -34,22 +34,26 @@ public class SupplierInfoModel {
   // 장비구매액
   private String purchase_price;
   
-  
   public String getPurchase_price() {
     return purchase_price;
   }
+  
   public void setPurchase_price(String purchase_price) {
     this.purchase_price = purchase_price;
   }
+  
   public String getL_ct_cd() {
     return l_ct_cd;
   }
+  
   public void setL_ct_cd(String l_ct_cd) {
     this.l_ct_cd = l_ct_cd;
   }
+  
   public String getSupply_cd() {
     return supply_cd;
   }
+  
   public void setSupply_cd(String supply_cd) {
     this.supply_cd = supply_cd;
   }
@@ -57,42 +61,55 @@ public class SupplierInfoModel {
   public String getSupply_nm() {
     return supply_nm;
   }
+  
   public void setSupply_nm(String supply_nm) {
     this.supply_nm = supply_nm;
   }
+  
   public String getSupply_mng_nm() {
     return supply_mng_nm;
   }
+  
   public void setSupply_mng_nm(String supply_mng_nm) {
     this.supply_mng_nm = supply_mng_nm;
   }
+  
   public String getTel() {
     return tel;
   }
+  
   public void setTel(String tel) {
     this.tel = tel;
   }
+  
   public String getMng_tel() {
     return mng_tel;
   }
+  
   public void setMng_tel(String mng_tel) {
     this.mng_tel = mng_tel;
   }
+  
   public String getEmail() {
     return email;
   }
+  
   public void setEmail(String email) {
     this.email = email;
   }
+  
   public String getWarehouse_cd() {
     return warehouse_cd;
   }
+  
   public void setWarehouse_cd(String warehouse_cd) {
     this.warehouse_cd = warehouse_cd;
   }
+  
   public String getWarehouse_nm() {
     return warehouse_nm;
   }
+  
   public void setWarehouse_nm(String warehouse_nm) {
     this.warehouse_nm = warehouse_nm;
   }
@@ -100,36 +117,47 @@ public class SupplierInfoModel {
   public String getProduct_cd() {
     return product_cd;
   }
+  
   public void setProduct_cd(String product_cd) {
     this.product_cd = product_cd;
   }
+  
   public String getProd_nm() {
     return prod_nm;
   }
+  
   public void setProd_nm(String prod_nm) {
     this.prod_nm = prod_nm;
   }
+  
   public String getL_ct_nm() {
     return l_ct_nm;
   }
+  
   public void setL_ct_nm(String l_ct_nm) {
     this.l_ct_nm = l_ct_nm;
   }
+  
   public int getStock() {
     return stock;
   }
+  
   public void setStock(int stock) {
     this.stock = stock;
   }
+  
   public int getPrice() {
     return price;
   }
+  
   public void setPrice(int price) {
     this.price = price;
   }
+  
   public String getM_ct_cd() {
     return m_ct_cd;
   }
+  
   public void setM_ct_cd(String m_ct_cd) {
     this.m_ct_cd = m_ct_cd;
   }
