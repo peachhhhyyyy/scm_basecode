c1accce86863bf2fc6cb5c231ab019d016ccee1f
wold21 <wold2180@gmail.com>
2021-05-27 09:38:50 +0900

    변경사항  - 필요없는 변수 및 해당 getter setter 제거

--- src/main/java/kr/happyjob/study/scm/model/ScmReturnListModel.java
+++ src/main/java/kr/happyjob/study/scm/model/ScmReturnListModel.java
@@ -17,14 +17,6 @@ public class ScmReturnListModel {
 	private String scm_nm;
 	// 제출일자 
 	private String submit_date;
-	// 창고명 
-	private String warehouse_nm;
-	// 반품 총금액 
-	private String refund_amt;
-	// 승인ID(임원ID)
-	private String approve_nm;
-	// 공급처명 
-	private String supply_nm;
 	
 	public String getRefund_list_no() {
 		return refund_list_no;
@@ -67,30 +59,6 @@ public class ScmReturnListModel {
 	}
 	public void setSubmit_date(String submit_date) {
 		this.submit_date = submit_date;
-	}
-	public String getWarehouse_nm() {
-		return warehouse_nm;
-	}
-	public void setWarehouse_nm(String warehouse_nm) {
-		this.warehouse_nm = warehouse_nm;
-	}
-	public String getRefund_amt() {
-		return refund_amt;
-	}
-	public void setRefund_amt(String refund_amt) {
-		this.refund_amt = refund_amt;
-	}
-	public String getApprove_nm() {
-		return approve_nm;
-	}
-	public void setApprove_nm(String approve_nm) {
-		this.approve_nm = approve_nm;
-	}
-	public String getSupply_nm() {
-		return supply_nm;
-	}
-	public void setSupply_nm(String supply_nm) {
-		this.supply_nm = supply_nm;
 	}
 	
 }
