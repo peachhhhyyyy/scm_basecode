0f6c7f2e182226f68c698d2518b5ec6eda06b97c
peachhhhyyyy <peachhhhyyyy@gmail.com>
2021-05-28 11:20:18 +0900

    모달창 띄우기 controller

--- src/main/java/kr/happyjob/study/scm/controller/EmployeeInfoController.java
+++ src/main/java/kr/happyjob/study/scm/controller/EmployeeInfoController.java
@@ -15,7 +15,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import kr.happyjob.study.pcs.model.PcsModel;
 import kr.happyjob.study.scm.model.EmployeeInfoModel;
 import kr.happyjob.study.scm.service.EmployeeInfoService;
 
@@ -39,12 +41,12 @@ public class EmployeeInfoController {
     return "scm/employeeInfo";
   }
   
-  // 직원정보 관리 목록 조회
+  // 직원정보관리 목록 조회
   @RequestMapping("employeeInfoList.do")
-  public String scmPurchaseList(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+  public String employeeInfoList(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
       HttpServletResponse response, HttpSession session) throws Exception {
     
-    logger.info("+ Start " + className + ".scmPurchaseList");
+    logger.info("+ Start " + className + ".employeeInfoList");
     logger.info("   - paramMap : " + paramMap);
     
     int currentPage = Integer.parseInt((String)paramMap.get("currentPage"));  // 현재 페이지 번호
@@ -56,18 +58,37 @@ public class EmployeeInfoController {
 
     logger.info("   - paramMap : " + paramMap);
 
-    // 발주지시서 목록 조회
+    // 직원정보관리 목록 조회
     List<EmployeeInfoModel> employeeInfoModel = employeeInfoService.employeeInfo(paramMap);
-    model.addAttribute("scmPurchaseOrderingOrderModel", employeeInfoModel);
+    model.addAttribute("employeeInfoModel", employeeInfoModel);
     
-    // 발주지시서 목록 카운트 조회
+    // 직원정보관리 목록 카운트 조회
     int totalCount =  employeeInfoService.countEmployeeInfo(paramMap);
     model.addAttribute("totalCount", totalCount);
     model.addAttribute("pageSize", pageSize);
     model.addAttribute("currentPage", currentPage);
     
-    logger.info("+ End " + className + ".scmPurchaseList");
+    logger.info("+ End " + className + ".employeeInfoList");
     
     return "scm/listEmployee";
   } 
+  
+  // 직원이름 클릭 시 모달창 띄우기
+  @RequestMapping("selectSubmitBtn.do")
+  @ResponseBody
+  public Map<String, Object> selectSubmitBtn(Model model, @RequestParam Map<String, Object> paramMap, HttpServletRequest request,
+      HttpServletResponse response, HttpSession session) throws Exception {
+    
+    logger.info("+ Start " + className + ".selectSubmitBtn");
+    logger.info("   - paramMap : " + paramMap);
+    
+    EmployeeInfoModel employeeInfoModel = employeeInfoService.selectSubmitBtn(paramMap);
+    
+    Map<String, Object> resultMap = new HashMap<String, Object>();
+    resultMap.put("employeeInfoModel", employeeInfoModel);
+
+    logger.info("+ End " + className + ".selectSubmitBtn");
+    
+    return resultMap;
+  }
 }
