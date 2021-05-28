9720da24ed0d63265cd0409a09e5dad668dc5eb7
peachhhhyyyy <peachhhhyyyy@gmail.com>
2021-05-28 11:21:21 +0900

    직원정보관리 ServiceImpl

--- src/main/java/kr/happyjob/study/scm/service/EmployeeInfoServicelmpl.java
+++ src/main/java/kr/happyjob/study/scm/service/EmployeeInfoServicelmpl.java
@@ -8,9 +8,8 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kr.happyjob.study.scm.dao.ScmPurchaseDao;
+import kr.happyjob.study.scm.dao.EmployeeInfoDao;
 import kr.happyjob.study.scm.model.EmployeeInfoModel;
-import kr.happyjob.study.scm.model.ScmPurchaseModel;
 
 @Service
 public class EmployeeInfoServicelmpl implements EmployeeInfoService {
@@ -22,41 +21,23 @@ public class EmployeeInfoServicelmpl implements EmployeeInfoService {
   private final String className = this.getClass().toString();
   
   @Autowired
-  ScmPurchaseDao scmPurchaseDao;
-
-  /** 발주지시서 목록 조회  */
-  public List<ScmPurchaseModel> scmPcsOrderingOrder(Map<String, Object> paramMap) {
-    List<ScmPurchaseModel> listPcsOrderingOrder = scmPurchaseDao.scmPcsOrderingOrder(paramMap);
-    return listPcsOrderingOrder;
+  EmployeeInfoDao employeeInfoDao;
+  
+  /** 직원정보관리 목록 조회  */
+  public List<EmployeeInfoModel> employeeInfo(Map<String, Object> paramMap) {
+    List<EmployeeInfoModel> employeeInfo = employeeInfoDao.employeeInfo(paramMap);
+    return employeeInfo;
   }
 
-  /** 발주지시서 목록 카운트 조회 */
-  public int countScmPcsOrderingOrder(Map<String, Object> paramMap) {
-    int totalCount = scmPurchaseDao.countScmPcsOrderingOrder(paramMap);
+  /** 직원정보관리 목록 카운트 조회 */
+  public int countEmployeeInfo(Map<String, Object> paramMap) {
+    int totalCount = employeeInfoDao.countEmployeeInfo(paramMap);
     return totalCount;
   }
   
-  /** 발주서 전송 누르고 조회 */
-  public ScmPurchaseModel selectPurchBtn(Map<String, Object> paramMap) {
-    ScmPurchaseModel scmPurchaseModel = scmPurchaseDao.selectPurchBtn(paramMap);
-    return scmPurchaseModel;
-  }
-
-  @Override
-  public List<EmployeeInfoModel> employeeInfo(Map<String, Object> paramMap) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public int countEmployeeInfo(Map<String, Object> paramMap) {
-    // TODO Auto-generated method stub
-    return 0;
-  }
-
-  @Override
+  /** 직원정보관리 단건 조회 */
   public EmployeeInfoModel selectSubmitBtn(Map<String, Object> paramMap) {
-    // TODO Auto-generated method stub
-    return null;
+    EmployeeInfoModel employeeInfoModel = employeeInfoDao.selectSubmitBtn(paramMap);
+    return employeeInfoModel;
   }
 }
