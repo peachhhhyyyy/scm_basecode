dcae902e9e42e6164a39669db825cd4253a9b74d
peachhhhyyyy <peachhhhyyyy@gmail.com>
2021-05-28 16:05:19 +0900

    안 쓰는 소스 제거

--- src/main/java/kr/happyjob/study/scm/controller/EmployeeInfoController.java
+++ src/main/java/kr/happyjob/study/scm/controller/EmployeeInfoController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import kr.happyjob.study.pcs.model.PcsModel;
 import kr.happyjob.study.scm.model.EmployeeInfoModel;
 import kr.happyjob.study.scm.service.EmployeeInfoService;
 
